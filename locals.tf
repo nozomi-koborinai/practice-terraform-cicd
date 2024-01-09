a102e4d2d7ef962c37464086b54e9b323cd79ed3
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2024-01-09 18:55:29 +0900

    feat: cicd

---
+++ locals.tf
@@ -0,0 +1,40 @@
+locals {
+  docs = {
+    "question-1" = {
+      collection  = "quiz"
+      document_id = "question-1"
+      fields      = jsonencode({
+        "question" = {
+          "stringValue" = "First Question"
+        },
+        "answer" = {
+          "stringValue" = "Answer 1"
+        }
+      })
+    },
+    "question-2" = {
+      collection  = "quiz"
+      document_id = "question-2"
+      fields      = jsonencode({
+        "question" = {
+          "stringValue" = "Second Question"
+        },
+        "answer" = {
+          "stringValue" = "Answer 2"
+        }
+      })
+    },
+    "question-3" = {
+      collection  = "quiz"
+      document_id = "question-3"
+      fields      = jsonencode({
+        "question" = {
+          "stringValue" = "Third Question"
+        },
+        "answer" = {
+          "stringValue" = "Answer 3"
+        }
+      })
+    },
+  }
+}
